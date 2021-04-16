58c640d1e0b41bfc4ca2a196b3c7c602aeff0ec1
liobio <582437980@qq.com>
2021-04-17 00:01:16 +0800

    over

--- 20.有效的括号.java
+++ 20.有效的括号.java
@@ -7,6 +7,36 @@
 // @lc code=start
 class Solution {
     public boolean isValid(String s) {
+        Stack<Character> stack=new Stack<>();
+        
+        for(int i = 0;i<s.length();i++){
+            char c = s.charAt(i);
+            switch (c) {
+            case ')':
+                if(!stack.isEmpty() && stack.pop() == '('){
+                    break;
+                }else{
+                    return false;
+                }
+            case ']':
+                if(!stack.isEmpty() && stack.pop() == '['){
+                    break;
+                }else{
+                    return false;
+                }
+            case '}':
+                if(!stack.isEmpty() && stack.pop() == '{'){
+                    break;
+                }else{
+                    return false;
+                }
+            default:
+                stack.push(c);
+                break;
+            }
+        }
+        return stack.isEmpty();
+    
 
     }
 }
